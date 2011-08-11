d66382aaf231bf0c0520114dc724cfa2ee3f9bfc
Gonçalo Silva <gnorsilva@gmail.com>
2011-08-11 15:30:11 +0100

    Adding test project for the android demo and changed the general pom

---
+++ imageLoaderDemoTests/src/com/novoda/lib/imageloadertester/test/ImageLoaderTestActivityTest.java
@@ -0,0 +1,16 @@
+package com.novoda.lib.imageloadertester.test;
+
+import com.novoda.lib.imageloadertester.ImageLoaderTestActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class ImageLoaderTestActivityTest extends ActivityInstrumentationTestCase2<ImageLoaderTestActivity>{
+
+	public ImageLoaderTestActivityTest() {
+		super("com.novoda.lib.imageloadertester",ImageLoaderTestActivity.class);
+	}
+
+	public void testSomething(){
+		assert(true);
+	}
+}
